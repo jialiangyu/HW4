4397086612660d98bfb259d24f9a5fcde5f58da2
Jialiang Yu <jialiang-yu@uiowa.edu>
2014-10-01 15:34:24 -0500

    add files to git hub

---
+++ Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+     
+    </div>
+        <asp:Label ID="lb_EmployeeWageCalculator" runat="server" Text="Employee Wage Calculator"></asp:Label>
+        <br />
+        <br />
+        Hourly Wage:<br />
+        <asp:TextBox ID="tb_hourlywage" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Working Hours:<br />
+        <asp:TextBox ID="tb_workinghours" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Pre-tax Deduction:<br />
+        <asp:TextBox ID="tb_pretaxdeduction" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        After-tax Deduction:<br />
+        <asp:TextBox ID="tb_aftertaxdeduction" runat="server"></asp:TextBox>
+        <br />
+        <br />
+
+        <asp:Button ID="PerformCalcButton" runat="server" Text="Calculate" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" Text="Clear" Width="86px" />
+        <br />
+        <br />
+&nbsp;<asp:Label ID="lb_results" runat="server"></asp:Label>
+        <br />
+        <br />
+    </form>
+</body>
+</html>
---
+++ Default.aspx.vb
@@ -0,0 +1,41 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub PerformCalcButton_Click(sender As Object, e As EventArgs) Handles PerformCalcButton.Click
+        Dim rate1 As Decimal = 0.18
+        Dim rate2 As Decimal = 0.22
+
+        ' Create variables to hold the values entered by the user
+        Dim hourlywage As Decimal = tb_hourlywage.Text
+        Dim workinghours As Decimal = tb_workinghours.Text
+        Dim pretax As Decimal = tb_pretaxdeduction.Text
+        Dim aftertax As Decimal = tb_aftertaxdeduction.Text
+
+        Dim totalwage As Decimal
+        totalwage = hourlywage * workinghours
+
+        Dim grosspay As Decimal
+        grosspay = totalwage - pretax
+
+        Dim finalwage As Decimal
+        If grosspay < 500 Then
+            finalwage = grosspay * (1 - rate1) - aftertax
+
+        Else
+            finalwage = grosspay * (1 - rate2) - aftertax
+        End If
+
+        ' Display the results in the results Label Web Control
+        lb_results.Text = "Your net pay for the week is $" & finalwage.ToString("#,####.##")
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        tb_aftertaxdeduction.Text = ""
+        tb_hourlywage.Text = ""
+        tb_pretaxdeduction.Text = ""
+        tb_workinghours.Text = ""
+        lb_results.Text = ""
+    End Sub
+End Class
